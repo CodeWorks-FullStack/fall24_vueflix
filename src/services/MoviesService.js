695b921ff472c0b7298e073e8940e1a422df83b2
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-17 11:44:29 -0600

    refactored service code

--- src/services/MoviesService.js
+++ src/services/MoviesService.js
@@ -7,21 +7,21 @@ class MoviesService {
   async getMovies() {
     const response = await movieAPI.get('discover/movie')
     console.log('GOT MOVIES 🎥🍿🎞️', response.data);
-    const newMovies = response.data.results.map(moviePOJO => new Movie(moviePOJO))
-    // REVIEW verify the data looks correct by using your Vue dev tools
-    AppState.movies = newMovies
-    AppState.currentPage = response.data.page
-    AppState.totalPages = response.data.total_pages
+    this.handleResponseData(response.data)
   }
 
   async changeMoviesPage(pageNumber) {
     const response = await movieAPI.get(`discover/movie?page=${pageNumber}`)
     console.log('CHANGED MOVIES PAGE 🍿🎥📃', response.data);
-    const newMovies = response.data.results.map(moviePOJO => new Movie(moviePOJO))
+    this.handleResponseData(response.data)
+  }
+
+  handleResponseData(responseData) {
+    const newMovies = responseData.results.map(moviePOJO => new Movie(moviePOJO))
     // REVIEW verify the data looks correct by using your Vue dev tools
     AppState.movies = newMovies
-    AppState.currentPage = response.data.page
-    AppState.totalPages = response.data.total_pages
+    AppState.currentPage = responseData.page
+    AppState.totalPages = responseData.total_pages
   }
 }
 
