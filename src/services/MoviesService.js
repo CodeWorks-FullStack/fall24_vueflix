b79feb7008dd12d1c71030c60c3c5ba9b3588475
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-17 11:37:30 -0600

    storing movies in appstate

--- src/services/MoviesService.js
+++ src/services/MoviesService.js
@@ -17,6 +17,11 @@ class MoviesService {
   async changeMoviesPage(pageNumber) {
     const response = await movieAPI.get(`discover/movie?page=${pageNumber}`)
     console.log('CHANGED MOVIES PAGE 🍿🎥📃', response.data);
+    const newMovies = response.data.results.map(moviePOJO => new Movie(moviePOJO))
+    // REVIEW verify the data looks correct by using your Vue dev tools
+    AppState.movies = newMovies
+    AppState.currentPage = response.data.page
+    AppState.totalPages = response.data.total_pages
   }
 }
 
